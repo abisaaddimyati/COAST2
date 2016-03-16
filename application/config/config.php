31fe4c224b783e621b3a4e5e4f3ee9ae8dc0b093
abisaaddimyati <dimyatiabisaad@gmail.com>
2016-03-16 19:43:51 +0700

    tes

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/COAST';
+$config['base_url']	= 'http://localhost/COAST2';
 
 /*
 |--------------------------------------------------------------------------
